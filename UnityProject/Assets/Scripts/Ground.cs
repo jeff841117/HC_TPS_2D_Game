0befc00477ca2d364f49484455e699db142d405a
jeff <jeff841117@gmail.com>
2019-11-02 20:59:31 +0800

    完成場景、介面與新增地板腳本

---
+++ UnityProject/Assets/Scripts/Ground.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    //宣告變數(定義欄位 field)
+    //欄位類型 欄位名稱 (指定 值) 結束
+    //private 私人(隱藏)，public 公開(顯示)
+    //int 整數
+    public int speed = 10;
+
+}
